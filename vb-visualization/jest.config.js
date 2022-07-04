cf7c820c34584c47c22d7f8e3c9ad6aba1238296
Bill Luu <billluu619@gmail.com>
2022-07-04 16:55:20 -0600

    test2

--- vb-visualization/jest.config.js
+++ vb-visualization/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   globals: {},
   testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.vue$": "vue-jest",
-    "^.+\\js$": "babel-jest"
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\js$': 'babel-jest'
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
 }
